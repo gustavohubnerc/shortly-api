544eb69fbe103238faaf9907cd167bf25edafe01
gustavohubnerc <hubnergc@gmail.com>
2023-08-04 14:13:38 -0300

    fix: shorten url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -10,12 +10,12 @@ export async function shortenUrl(req, res){
 
         const token = authorization.replace("Bearer ", "");
 
-        const user = await db.query(`SELECT userId FROM sessions WHERE token = $1`, [token]);
-        if(!user.rows[0]) return res.status(401).send("Unauthorized");
+        const user = await db.query(`SELECT "userId" FROM sessions WHERE token = $1`, [token]);
+        if(user.rowCount === 0) return res.status(401).send("Unauthorized");
         
         const shortUrl = nanoid(8);
 
-        const userId = user.rows[0].id;
+        const userId = user.rows[0].userId;
 
         const insertUrl = await db.query(`INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3) RETURNING id`, [url, shortUrl, userId]);
         const id = insertUrl.rows[0].id;
