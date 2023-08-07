cc50526b25b9b15579db617eb3dc25a728a3d28a
gustavohubnerc <hubnergc@gmail.com>
2023-08-07 10:24:58 -0300

    feat: users info

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,37 @@
+import { db } from "../database/database.connection.js";
+
+export async function getUserInfo(req, res){
+    const authorization = req.headers.authorization;
+    if(!authorization) return res.status(401).send("Unauthorized");
+    try {
+        const token = authorization.replace("Bearer ", "");
+
+        const user = await db.query(`SELECT "userId" FROM sessions WHERE token = $1`, [token]); 
+        if (user.rowCount === 0) return res.status(401).send('User not found');
+
+        const userName = await db.query(`SELECT name FROM users WHERE id = $1`, [user.rows[0].userId]);
+
+        const visitors = await db.query(`SELECT SUM(visits) AS totalviews FROM urls WHERE "userId" = $1`, [user.rows[0].userId]);
+        const visitCount = visitors.rows[0].totalviews;
+
+        const urls = await db.query(`SELECT id, url, "shortUrl" FROM urls WHERE "userId" = $1`, [user.rows[0].userId]);
+
+        const shortUrls = urls.rows.map((url) => ({
+            id: url.id,
+            shortUrl: url.shortUrl,
+            url: url.url,
+            visitors: url.visits,
+        }));
+
+        const userInfo = {
+            id: user.rows[0].userId,
+            name: userName.rows[0].name,
+            visitCount,
+            shortUrls
+        };
+
+        res.status(200).send(userInfo);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getUserInfo } from "../controllers/users.controllers.js";
 
 const usersRouter = Router();
+
+usersRouter.get("/users/me", getUserInfo);
 
 export default usersRouter;
